7640dfe19a3b51761383f2932e1356721969e70d
platinumpotato <58384351+platinumpotato@users.noreply.github.com>
2020-07-19 14:38:49 +0700

    inital commit

---
+++ project/main.js
@@ -0,0 +1,5 @@
+// let backgroundImg = document.getElementById('home-page-img')
+// let homePgContent = document.getElementById('home-page-content')
+// let aboutUsTitle = document.getElementById('about-us-title')
+// aboutUsTitle.style.marginTop = backgroundImg.offsetHeight
+// // homePgContent.style.height = backgroundImg.offsetHeight

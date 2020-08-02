bd4a29c885e9e3835bbdaa62fb460f4272b179e0
platinumpotato <58384351+platinumpotato@users.noreply.github.com>
2020-08-02 16:14:17 +0700

    made progress

--- project/main.js
+++ project/main.js
@@ -1,5 +1,11 @@
-// let backgroundImg = document.getElementById('home-page-img')
-// let homePgContent = document.getElementById('home-page-content')
-// let aboutUsTitle = document.getElementById('about-us-title')
-// aboutUsTitle.style.marginTop = backgroundImg.offsetHeight
-// // homePgContent.style.height = backgroundImg.offsetHeight
+let lop1 = document.getElementById('lop-1')
+let lop2 = document.getElementById('lop-2')
+let lop3 = document.getElementById('lop-3')
+let lop4 = document.getElementById('lop-4')
+let lop5 = document.getElementById('lop-5')
+lop1.addEventListener('click', dangToan1)
+function dangToan1() {
+    lop5.style.display = 'none'
+    lop4.style.display = 'none'
+    lop3.style.display = 'none'
+}
